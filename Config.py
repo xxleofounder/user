9abbd4c2d46fe73a2fc33bb8b9533dfac74fbe04
xxleofounder <ahmetnurullah033@gmail.com>
2025-09-11 14:02:19 +0300

    Update Config.py

--- Config.py
+++ Config.py
@@ -6,4 +6,4 @@ class Config():
     API_HASH = "2b4f554c9819d4c1e7384da849318635"
 
     # token
-    BOT_TOKEN = ":8209712439:AAFrWkHVww7EnU-3LDUBDjeeA758y8FkZOk" 
+    BOT_TOKEN = "8209712439:AAH-TuZmh0ou1sUw5KtT7Ebox4Av7lQIkFg" 
