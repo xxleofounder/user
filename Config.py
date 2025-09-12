f2175effac8c6671c3d4d9eae7ab0909f694a2e9
xxleofounder <ahmetnurullah033@gmail.com>
2025-09-12 21:47:08 +0300

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,9 +1,13 @@
+# T.ME//ARTZFOUNDER
+
 import os
 
 class Config():
     # my.telegram.org'dan alacağın bilgiler
-    API_ID = 26719067  # int olarak yaz
+    API_ID = 26719067 
     API_HASH = "2b4f554c9819d4c1e7384da849318635"
-
-    # token
+    
+    BOT_USERNAME = "leousertaggerbot"
+    OWNER_USER = "artzfounder"
+    OWNER_ID = 8276543841
     BOT_TOKEN = "8209712439:AAH-TuZmh0ou1sUw5KtT7Ebox4Av7lQIkFg" 
