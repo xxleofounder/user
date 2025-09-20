a363d8c7e7d49eafb3a8fe902dde0e87e3715972
xxleofounder <ahmetnurullah033@gmail.com>
2025-09-20 15:23:51 +0300

    Update Config.py

--- Config.py
+++ Config.py
@@ -7,10 +7,10 @@ class Config():
     API_ID = 26719067 
     API_HASH = "2b4f554c9819d4c1e7384da849318635"
     
-    BOT_USERNAME = "leousertaggerbot"
+    BOT_USERNAME = "FundaEtiketbot"
     OWNER_USER = "artzfounder"
     OWNER_ID = 8276543841
-    BOT_TOKEN = "8209712439:AAH-TuZmh0ou1sUw5KtT7Ebox4Av7lQIkFg" 
+    BOT_TOKEN = "8028209670:AAEIHCufjACXO730EzIi7wRsPx3PQZJ2v5E" 
     BOT_NAME = "Funda"
     
 
