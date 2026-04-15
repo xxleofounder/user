360537a9a714e2ee570c7a1e4ef3611de43dbb01
xxleofounder <ahmetnurullah033@gmail.com>
2026-04-15 22:06:38 +0300

    Add AFK status variables to shared.py

--- shared.py
+++ shared.py
@@ -10,3 +10,9 @@ chat_memories = {}
 
 # Botun başlangıç zamanı
 start_time = time.time()
+
+# shared.py içine eklenecekler
+is_afk = False
+afk_reason = ""
+afk_time = 0
+afk_mentions = [] # Sen yokken seni etiketleyenlerin listesi
